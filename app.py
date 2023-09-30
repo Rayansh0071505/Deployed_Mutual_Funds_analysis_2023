773bd8039730ceae574bbcc820e4f836cbb06e0a
Rayansh <rayanshsrivastava1@gmail.com>
2023-10-01 01:34:27 +0530

    new push

--- app.py
+++ app.py
@@ -131,8 +131,8 @@ def return_one_bar():
     ax.set_ylabel('Returns in percentage', fontsize=24, fontname="Comic Sans MS")
     # ax.set_title('3 Year Returns(%)', y=1.20, fontsize=24)
 
-    ax.set_xticks(X)
-    ax.set_xticklabels(X, rotation='vertical')
+    ax.set_xticks(range(len(X)))
+    ax.set_xticklabels(X, rotation=90)  
     ax.grid()
 
     for bar in bars:
@@ -239,8 +239,8 @@ def return_3yrs_graph():
     ax.legend(labels, loc='upper right', fontsize=15)
 
     # Rotating x-axis labels and adding a grid
-    ax.set_xticks(X, rotation='vertical')
-    ax.set_xticklabels(X, rotation='vertical')
+    ax.set_xticks(range(len(X)))
+    ax.set_xticklabels(X, rotation=90) 
     ax.grid()
 
     for i, j in zip(X, returns):
